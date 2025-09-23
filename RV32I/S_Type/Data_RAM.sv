74b2f16d4c3386e94232362ad0dd5128292f633c
Jiyun_Han <162584369+J-HanRyang@users.noreply.github.com>
2025-09-23 15:43:43 +0900

    Update Data_RAM.sv

--- RV32I/S_Type/Data_RAM.sv
+++ RV32I/S_Type/Data_RAM.sv
@@ -10,7 +10,8 @@
 // Tool Versions    : 2020.2
 // Description      : Data_RAM
 //
-// Revision 	    : 
+// Revision 	    : 2025/09/22    RAM_Upgrade V1.1(Add Type Select)
+//                  : 2025/09/23    Word Align -> Byte Align
 //////////////////////////////////////////////////////////////////////////////////
 
 module Data_RAM(
@@ -24,14 +25,54 @@ module Data_RAM(
     output  logic   [31:0]  oData_RdData
     );
 
-    logic   [31:0]  rData_Mem[0:15];
+    logic   [31:0]  rData_Mem[0:31];
+    logic   [31:2]  wAddr_Word;
+    logic   [1:0]   wAddr_Byte;
+    logic   [31:0]  wRaw_RdData;
+
+    assign  wAddr_Word  = iData_Addr[31:2];
+    assign  wAddr_Byte  = iData_Addr[1:0];
+    assign  wRaw_RdData = rData_Mem[wAddr_Word];
+
+    initial
+    begin
+        for (int i = 0; i < 32 ; i++)
+        begin
+            rData_Mem[i] = i + 32'h1234_8000;
+        end
+    end
 
     always_ff @(posedge iClk)
     begin
         if (iData_WrEn)
-            rData_Mem[iData_Addr] <= iData_WrData;
+            case (iFunct3)
+                3'b000  : 
+                begin
+                    case (wAddr_Byte)
+                        2'b00   : rData_Mem[wAddr_Word][7:0]    <= iData_WrData[7:0];
+                        2'b01   : rData_Mem[wAddr_Word][15:8]   <= iData_WrData[7:0];
+                        2'b10   : rData_Mem[wAddr_Word][23:16]  <= iData_WrData[7:0];
+                        2'b11   : rData_Mem[wAddr_Word][31:24]  <= iData_WrData[7:0];
+                        default : rData_Mem[wAddr_Word][7:0]    <= iData_WrData[7:0];
+                    endcase
+                end
+
+                3'b001  : 
+                begin
+                    case (wAddr_Byte[1])
+                        1'b0    : rData_Mem[wAddr_Word][15:0]   <= iData_WrData[15:0];
+                        1'b1    : rData_Mem[wAddr_Word][31:16]  <= iData_WrData[15:0];
+                        default : rData_Mem[wAddr_Word][15:0]   <= iData_WrData[15:0];
+                    endcase
+                end
+
+                3'b010  :
+                    rData_Mem[wAddr_Word] <= iData_WrData;
+
+                default : ;
+            endcase
     end
 
-    assign  oData_RdData = rData_Mem[iData_Addr];
+    assign  oData_RdData = rData_Mem[wAddr_Word];
 
 endmodule
